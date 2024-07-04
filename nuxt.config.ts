bb645bc339a0b942f8f939e8d8b5928f69fed069
Thandi M. <TMenelas@outlook.com>
2024-07-04 01:22:47 -0400

    Updated Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,18 @@ export default defineNuxtConfig({
 	modules: [
 		"@nuxtjs/tailwindcss", 
 		"@nuxt/icon"
-	]
-})
+	],
+	app: {
+		head: {
+			title: "Thandi R. Menelas - RealStr1ke",
+			meta: [
+				{ charset: "utf-8" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{ hid: "description", name: "description", content: "" },
+			],
+			link: [
+				{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
+			],
+		},
+	}
+});
