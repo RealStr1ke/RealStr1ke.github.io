85ab399a59f6e9b1604484b99c717248410e29fb
Thandi <TMenelas@outlook.com>
2025-10-26 05:00:41 -0400

    Update age in ProfileCard description

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -16,7 +16,7 @@
 					py-3"
           >Click to check out my GitHub!</div>
           <div class="divider" />
-          <p class="mb-2 text-2xl">I'm a 17-year-old self-taught full-stack developer / dual-enrolled high school student.</p>
+          <p class="mb-2 text-2xl">I'm a 18-year-old self-taught full-stack developer.</p>
           <p class="mb-5 text-xl">I work mainly with Linux operating systems and I mostly use JavaScript at the moment (although I know Python, Java, and more).</p>
           <p class="badge text-lg h-auto py-1">I'm currently rebuilding this website, so please come back soon!</p>
         </div>
